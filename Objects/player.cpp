a1f23158340b724e8c9ee1b2ff44c939c51f2eb8
skobivan <56694548+skobivan@users.noreply.github.com>
2020-01-26 01:59:47 +0300

    add animation for player

--- Objects/player.cpp
+++ Objects/player.cpp
@@ -2,9 +2,15 @@
 
 Player::Player(Vector2f pos, float speed)
 {
+    cout << "Trying to load tileset" << endl;
+    if(!img.loadFromFile("C:/Programming/qt_projects/2DEngine/Resources/Images/Knight_tileset.png")) {
+        cout << "can't load tileset" << endl;
+    } else cout << "load tileset" << endl;
+    texture.loadFromImage(img);
+    sprite.setTexture(texture);
+    sprite.setTextureRect(IntRect(0, 0, 14, 16));
+    sprite.scale(3, 3);
     sprite.setPosition(pos);
-    sprite.setSize(Vector2f(40, 40));
-    sprite.setFillColor(Color::Blue);
     SPEED = speed;
     addDamagedObj(new LaserGun(getCenter()));
 //    addDamagedObj(new Light(pos, 90));
@@ -22,15 +28,46 @@ void Player::logic()
 {
     for (auto it : damagedObjs) it->logic();
     float speed = SPEED * Globals::gameTime;
+    if (Mouse::getPosition(*Globals::mainWindow).x + Globals::mainWindow->getView().getCenter().x - Globals::mainWindow->getView().getSize().x/2 > getCenter().x) dir = eDir::RIGHT;
+    else dir = eDir::LEFT;
+    bool isMove = Keyboard::isKeyPressed(Keyboard::W)
+            || Keyboard::isKeyPressed(Keyboard::S)
+            || Keyboard::isKeyPressed(Keyboard::A)
+            || Keyboard::isKeyPressed(Keyboard::D);
     if (Keyboard::isKeyPressed(Keyboard::W)) sprite.move(0, -speed);
     if (Keyboard::isKeyPressed(Keyboard::S)) sprite.move(0, speed);
     if (Keyboard::isKeyPressed(Keyboard::A)) sprite.move(-speed, 0);
     if (Keyboard::isKeyPressed(Keyboard::D)) sprite.move(speed, 0);
+    if (isMove) {
+        aNTime += Globals::gameTime;
+        if (aNTime - prevAnTime > 80) {
+            textNum = (textNum+1)%4;
+            prevAnTime = aNTime;
+            if (dir == eDir::RIGHT) {
+                if (textNum == 0 || textNum == 3) sprite.move(-3, 0);
+                if (textNum == 1 || textNum == 2) sprite.move(3, 0);
+            }
+        }
+        if (dir == eDir::LEFT) {
+            sprite.setTextureRect(animation[1][textNum]);
+        } else {
+            sprite.setTextureRect(animation[0][textNum]);
+        }
+    } else {
+        if (dir == eDir::LEFT) {
+            sprite.setTextureRect(stand[1]);
+        } else {
+            sprite.setTextureRect(stand[0]);
+        }
+        aNTime = 0;
+        prevAnTime = 0;
+        textNum = 0;
+    }
 }
 
 Vector2f Player::getCenter()
 {
-    return Vector2f(sprite.getPosition().x + sprite.getSize().x/2, sprite.getPosition().y + sprite.getSize().y/2);
+    return Vector2f(sprite.getPosition().x + sprite.getTexture()->getSize().x/2, sprite.getPosition().y + sprite.getTexture()->getSize().y/2);
 }
 
 FloatRect Player::getGlobalBounds()
--- Objects/player.h
+++ Objects/player.h
@@ -1,14 +1,28 @@
-    #ifndef PLAYER_H
+#ifndef PLAYER_H
 #define PLAYER_H
 
 #include "unit.h"
 #include "globals.h"
 #include "lasergun.h"
 
+enum class eDir {LEFT, RIGHT};
+
 class Player : public Unit
 {
 private:
-    RectangleShape sprite;
+    eDir dir = eDir::LEFT;
+    Sprite sprite;
+    const IntRect animation[2][4] = {
+        {IntRect(14, 0, 14, 16), IntRect(28, 0, 13, 16), IntRect(41, 0, 12, 16), IntRect(53, 0, 13, 16)},
+        {IntRect(14, 16, 14, 16), IntRect(28, 16, 13, 16), IntRect(41, 16, 12, 16), IntRect(53, 16, 13, 16)}
+    };
+    const IntRect stand[2] = {IntRect(0, 0, 14, 16), IntRect(0, 16, 14, 16)};
+    Image img;
+    Texture texture;
+    int textNum = 0;
+    float playerTime = 0;
+    float prevAnTime = 0;
+    float aNTime = 0;
     // Drawable interface
 protected:
     void draw(RenderTarget &target, RenderStates states) const override;
